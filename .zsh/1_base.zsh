81305f4284bed24182eb8718d59576e11a2b8ece
kawacho <osm.kawachi@me.com>
2018-11-24 00:14:34 +0900

    split zshrc's prompt.

--- .zsh/1_base.zsh
+++ .zsh/1_base.zsh
@@ -1,18 +1,3 @@
-autoload -Uz colors
-colors
-PROMPT='%{$fg[blue]%}%n@%~%# %{$reset_color%}'
-
-autoload -Uz vcs_info
-setopt prompt_subst
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
-zstyle ':vcs_info:*' actionformats '[%b|%a]'
-precmd () { vcs_info }
-#RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
-RPROMPT='${vcs_info_msg_0_}'
-
 # 日本語ファイル名を表示可能にする
 setopt print_eight_bit
 # cdなしでディレクトリ移動
