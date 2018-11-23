04f881a654a8dcb9833ce5663d180c0cdf9372b9
kawacho <osm.kawachi@me.com>
2018-11-23 22:08:44 +0900

    change git to hub

--- setup.sh
+++ setup.sh
@@ -5,19 +5,19 @@ brew install reattach-to-user-namespace
 
 # git clone
 if [ ! -d ~/.goenv ]; then
-    hub clone syndbg/goenv.git ~/.goenv
+    hub clone syndbg/goenv ~/.goenv
 fi
 if [ ! -d ~/.pyenv ]; then
-    hub clone pyenv/pyenv.git ~/.pyenv
+    hub clone pyenv/pyenv ~/.pyenv
 fi
 if [ ! -d ~/.pyenv/plugins/pyenv-virtualenv ]; then
-    hub clone pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    hub clone pyenv/pyenv-virtualenv ~/.pyenv/plugins/pyenv-virtualenv
 fi
 if [ ! -d ~/.ndenv ]; then
     hub clone riywo/ndenv ~/.ndenv
 fi
 if [ ! -d ~/.ndenv/plugins/node-build ]; then
-    hub clone riywo/node-build.git ~/.ndenv/plugins/node-build
+    hub clone riywo/node-build ~/.ndenv/plugins/node-build
 fi
 
 sudo cp ./.tmux/bin/wifi /usr/local/bin/wifi
