e9780a62e4ac0a6c9753d75a907b2d55f99e011a
kawacho <osm.kawachi@me.com>
2018-11-23 19:43:49 +0900

    check existing dir & added cheets.md

--- cheets.md
+++ cheets.md
@@ -1,4 +1,21 @@
 # vim
+## Cursor
+- w/W : 次の単語の頭
+- e/E : 次の単語の末
+- b/B : 前の単語の頭
+
+- H/M/L : ウィンドウ内で上中下段移動
+
+## Scroll
+- Ctrl-b : 上に一画面
+- Ctrl-y : 上に一行
+- Ctrl-e : 下に一行
+- Ctrl-f : 下に一画面
+
+## Edit
+- Ctrl-p/n : 単語補完
+- . : 直前の編集の繰り返し
+- u/Ctrl-r : UNDO/REDO
 
 # tmux
 ## COPY MODE
--- install.sh
+++ install.sh
@@ -2,13 +2,23 @@
 brew install hub
 brew install tmux
 brew install reattach-to-user-namespace
-brew install goenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
-git clone https://github.com/riywo/ndenv ~/.ndenv
-git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+# git clone
+if [ ! -d ~/.goenv ]; then
+    git clone https://github.com/syndbg/goenv.git ~/.goenv
+fi
+if [ ! -d ~/.pyenv ]; then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+fi
+if [ ! -d ~/.pyenv/plugins/pyenv-virtualenv ]; then
+    git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+fi
+if [ ! -d ~/.ndenv ]; then
+    git clone https://github.com/riywo/ndenv ~/.ndenv
+fi
+if [ ! -d ~/.ndenv/plugins/node-build ]; then
+    git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+fi
 
 sudo cp ./.tmux/bin/wifi /usr/local/bin/wifi
 sudo cp ./.tmux/bin/battery /usr/local/bin/battery
-
