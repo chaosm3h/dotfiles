dc322a10db569388a7bd0d05f0e8cc3af664c4b0
chaosm3h <chaosm3h@gmail.com>
2019-10-18 11:40:13 +0900

    reorder git aliases

--- .zsh/5_exports.zsh
+++ .zsh/5_exports.zsh
@@ -7,12 +7,12 @@ export XDG_DATA_HOME=${HOME}/.local/share
 #export LDFLAGS="-L/usr/local/opt/gettext/lib"
 #export CPPFLAGS="-I/usr/local/opt/gettext/include"
 
-#export LDFLAGS="-L/usr/local/opt/readline/lib"
-#export CPPFLAGS="-I/usr/local/opt/readline/include"
+export LDFLAGS="-L/usr/local/opt/readline/lib"
+export CPPFLAGS="-I/usr/local/opt/readline/include"
 
-#export PATH="/usr/local/opt/sqlite/bin:$PATH"
-#export LDFLAGS="-L/usr/local/opt/sqlite/lib"
-#export CPPFLAGS="-I/usr/local/opt/sqlite/include"
+export PATH="/usr/local/opt/sqlite/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/sqlite/lib"
+export CPPFLAGS="-I/usr/local/opt/sqlite/include"
 eval $(dircolors ~/src/other/dircolors-solarized/dircolors.ansi-dark)
 
 # jumanpp
