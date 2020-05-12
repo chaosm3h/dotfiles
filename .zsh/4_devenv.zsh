0a0eeffc6831570ec7d9fd8385192746c74d8e32
chaosm3h <chaosm3h@gmail.com>
2020-05-12 18:11:26 +0900

    typo

--- .zsh/4_devenv.zsh
+++ .zsh/4_devenv.zsh
@@ -1,5 +1,5 @@
 ### nvm ###
-export NVM_DIR="$HOME/.config/.nvm"
+export NVM_DIR="$HOME/.config/nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
