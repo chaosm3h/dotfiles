df01b9dabaaa13bec86d46497db865278756ad52
chaosm3h <chaosm3h@gmail.com>
2020-09-06 17:32:16 +0900

    add ghc

--- .config/zsh/4_devenv.zsh
+++ .config/zsh/4_devenv.zsh
@@ -11,13 +11,8 @@ export NVM_DIR="$HOME/.config/nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-### golang environment ###
-#export GOENV_ROOT="${HOME}/.goenv"
-#if [ -d "${GOENV_ROOT}" ]; then
-#    export PATH=${GOENV_ROOT}/bin:$PATH
-#    export GOPATH=${HOME}/go
-#    eval "$(goenv init -)"
-#fi
+export GHCUP_DIR="$HOME/.ghcup/env"
+[ -f GHC_DIR ] && source GHC_DIR # ghcup-env
 
 ### direnv ###
 export EDITOR=/usr/bin/vim
